d854a48146f709547aece10c1656a45e574f28c0
Aidana <aidanaalymbekova2@gmail.com>
2023-03-16 20:31:15 +0600

    project1

--- outputs.tf
+++ outputs.tf
@@ -3,6 +3,9 @@ output "ami" {
 }
 
 output "instance_ip" {
-  value = aws_instance.aid-instance.*.public_ip
+  value = aws_instance.aid-instance.public_ip
 }
 
+output "aws_s3_bucket" {
+  value = aws_s3_bucket.my_bucket
+}
--- provider.tf
+++ provider.tf
@@ -1,11 +1,12 @@
 provider "aws" {
   region = "us-east-1"
+  alias = "east1"
 }
 
-
-
-
-
+provider "aws" {
+  alias = "east2"
+  region = "us-east-2"
+}
 
 
 
